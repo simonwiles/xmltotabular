613172c7ef4f817a6e68ea392169d67813f092df
Simon Wiles <simonjwiles@gmail.com>
2021-06-18 09:52:52 -0700

    Add test_multiple_docs_in_multiple_files

--- tests/test_xmlcollectiontotabular.py
+++ tests/test_xmlcollectiontotabular.py
@@ -47,3 +47,48 @@ def test_multiple_docs_in_one_file(simple_config):
             "released": "1972",
         },
     ]
+
+
+def test_multiple_docs_in_multiple_files(simple_config):
+
+    xml_path = "tests/test_xml_files/one_doc_per_file/*.xml"
+
+    collectionTransformer = XmlCollectionToTabular(
+        xml_path,
+        simple_config,
+        ":memory:",
+        "sqlite",
+        validate=False,
+        processes=1,
+        continue_on_error=False,
+        log_level=logging.DEBUG,
+    )
+
+    db = collectionTransformer.convert()
+    db.conn.row_factory = sqlite3.Row
+
+    result = [dict(row) for row in db.execute("SELECT * FROM album;").fetchall()]
+
+    assert result == [
+        {
+            "artist": "Nick Drake",
+            "genre": "Folk",
+            "label": "Island",
+            "name": "Five Leaves Left",
+            "released": "1969",
+        },
+        {
+            "artist": "Nick Drake",
+            "genre": "Folk",
+            "label": "Island",
+            "name": "Bryter Layter",
+            "released": "1971",
+        },
+        {
+            "artist": "Nick Drake",
+            "genre": "Folk",
+            "label": "Island",
+            "name": "Pink Moon",
+            "released": "1972",
+        },
+    ]
